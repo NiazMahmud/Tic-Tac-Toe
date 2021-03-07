e29476a2e7f75cb6236209239b80e3528a870b23
NiazMahmud <s.m.mahmud@northsouth.edu>
2021-03-07 21:46:08 +0600

    1st update

---
+++ player.py
@@ -0,0 +1,28 @@
+import math
+import random
+
+
+class Player:
+    def __init__(self, letter):
+        # letter is X or O
+        self.letter = letter
+
+
+    def get_move(self, game):
+        pass
+
+class RandmonComputerPlayer(Player):
+    def __init__(self,letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self,letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+
